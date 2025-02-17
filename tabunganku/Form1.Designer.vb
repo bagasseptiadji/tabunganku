0e3828e0e247767ef49dbdfef9e4abfe8a35d82a
Bagas Dwi <Bagas Dwi@IISM032481>
2025-02-17 15:56:29 +0700

    Add project files.

---
+++ tabunganku/Form1.Designer.vb
@@ -0,0 +1,334 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        dgvRiwayat = New DataGridView()
+        lblSaldo = New Label()
+        txtJumlah = New TextBox()
+        btnTambah = New Button()
+        btnRefresh = New Button()
+        ofdBukaFile = New OpenFileDialog()
+        btnHapus = New Button()
+        txtKeterangan = New TextBox()
+        lblInput = New Label()
+        lblKeterangan = New Label()
+        Label2 = New Label()
+        Label1 = New Label()
+        GroupBox1 = New GroupBox()
+        lblFileName = New Label()
+        Label7 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        cmbTransaksi = New ComboBox()
+        btnInfo = New Button()
+        btnFilter = New Button()
+        Label3 = New Label()
+        txtFilter = New TextBox()
+        Label4 = New Label()
+        btnBuatBaru = New Button()
+        btnBukaFile = New Button()
+        SaveFileDialog = New SaveFileDialog()
+        CType(dgvRiwayat, ComponentModel.ISupportInitialize).BeginInit()
+        GroupBox1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' dgvRiwayat
+        ' 
+        dgvRiwayat.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dgvRiwayat.Location = New Point(23, 30)
+        dgvRiwayat.Name = "dgvRiwayat"
+        dgvRiwayat.RowHeadersWidth = 62
+        dgvRiwayat.Size = New Size(813, 225)
+        dgvRiwayat.TabIndex = 0
+        ' 
+        ' lblSaldo
+        ' 
+        lblSaldo.AutoSize = True
+        lblSaldo.Font = New Font("Segoe UI", 15F)
+        lblSaldo.Location = New Point(859, 102)
+        lblSaldo.Name = "lblSaldo"
+        lblSaldo.Size = New Size(0, 41)
+        lblSaldo.TabIndex = 1
+        ' 
+        ' txtJumlah
+        ' 
+        txtJumlah.Location = New Point(164, 319)
+        txtJumlah.Name = "txtJumlah"
+        txtJumlah.Size = New Size(206, 31)
+        txtJumlah.TabIndex = 2
+        ' 
+        ' btnTambah
+        ' 
+        btnTambah.Location = New Point(33, 433)
+        btnTambah.Name = "btnTambah"
+        btnTambah.Size = New Size(99, 66)
+        btnTambah.TabIndex = 3
+        btnTambah.Text = "Tambah"
+        btnTambah.UseVisualStyleBackColor = True
+        ' 
+        ' btnRefresh
+        ' 
+        btnRefresh.Location = New Point(271, 433)
+        btnRefresh.Name = "btnRefresh"
+        btnRefresh.Size = New Size(99, 66)
+        btnRefresh.TabIndex = 5
+        btnRefresh.Text = "Refresh Data"
+        btnRefresh.UseVisualStyleBackColor = True
+        ' 
+        ' ofdBukaFile
+        ' 
+        ofdBukaFile.FileName = "OpenFileDialog1"
+        ' 
+        ' btnHapus
+        ' 
+        btnHapus.Location = New Point(152, 433)
+        btnHapus.Name = "btnHapus"
+        btnHapus.Size = New Size(99, 66)
+        btnHapus.TabIndex = 8
+        btnHapus.Text = "Hapus"
+        btnHapus.UseVisualStyleBackColor = True
+        ' 
+        ' txtKeterangan
+        ' 
+        txtKeterangan.Location = New Point(164, 377)
+        txtKeterangan.Name = "txtKeterangan"
+        txtKeterangan.Size = New Size(206, 31)
+        txtKeterangan.TabIndex = 9
+        ' 
+        ' lblInput
+        ' 
+        lblInput.AutoSize = True
+        lblInput.Location = New Point(26, 319)
+        lblInput.Name = "lblInput"
+        lblInput.Size = New Size(114, 25)
+        lblInput.TabIndex = 10
+        lblInput.Text = "Input Jumlah"
+        ' 
+        ' lblKeterangan
+        ' 
+        lblKeterangan.AutoSize = True
+        lblKeterangan.Location = New Point(26, 377)
+        lblKeterangan.Name = "lblKeterangan"
+        lblKeterangan.Size = New Size(106, 25)
+        lblKeterangan.TabIndex = 11
+        lblKeterangan.Text = "Keterangan "
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(142, 319)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(16, 25)
+        Label2.TabIndex = 12
+        Label2.Text = ":"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(142, 377)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(16, 25)
+        Label1.TabIndex = 13
+        Label1.Text = ":"
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(lblFileName)
+        GroupBox1.Controls.Add(Label7)
+        GroupBox1.Controls.Add(Label5)
+        GroupBox1.Controls.Add(Label6)
+        GroupBox1.Controls.Add(cmbTransaksi)
+        GroupBox1.Controls.Add(btnInfo)
+        GroupBox1.Controls.Add(btnFilter)
+        GroupBox1.Controls.Add(Label3)
+        GroupBox1.Controls.Add(txtFilter)
+        GroupBox1.Controls.Add(Label4)
+        GroupBox1.Controls.Add(btnBuatBaru)
+        GroupBox1.Controls.Add(btnBukaFile)
+        GroupBox1.Controls.Add(dgvRiwayat)
+        GroupBox1.Controls.Add(Label1)
+        GroupBox1.Controls.Add(lblSaldo)
+        GroupBox1.Controls.Add(btnRefresh)
+        GroupBox1.Controls.Add(Label2)
+        GroupBox1.Controls.Add(txtJumlah)
+        GroupBox1.Controls.Add(lblKeterangan)
+        GroupBox1.Controls.Add(btnTambah)
+        GroupBox1.Controls.Add(lblInput)
+        GroupBox1.Controls.Add(btnHapus)
+        GroupBox1.Controls.Add(txtKeterangan)
+        GroupBox1.Location = New Point(24, 12)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(1096, 525)
+        GroupBox1.TabIndex = 14
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Tabunganku"
+        ' 
+        ' lblFileName
+        ' 
+        lblFileName.AutoSize = True
+        lblFileName.Font = New Font("Segoe UI", 9F)
+        lblFileName.Location = New Point(859, 35)
+        lblFileName.Name = "lblFileName"
+        lblFileName.Size = New Size(0, 25)
+        lblFileName.TabIndex = 25
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI", 9F)
+        Label7.Location = New Point(859, 70)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(135, 25)
+        Label7.TabIndex = 24
+        Label7.Text = "Total Tabungan:"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(142, 273)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(16, 25)
+        Label5.TabIndex = 23
+        Label5.Text = ":"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(26, 273)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(63, 25)
+        Label6.TabIndex = 22
+        Label6.Text = "Pilihan"
+        ' 
+        ' cmbTransaksi
+        ' 
+        cmbTransaksi.FormattingEnabled = True
+        cmbTransaksi.Items.AddRange(New Object() {"Menabung", "Penarikan"})
+        cmbTransaksi.Location = New Point(164, 270)
+        cmbTransaksi.Name = "cmbTransaksi"
+        cmbTransaksi.Size = New Size(206, 33)
+        cmbTransaksi.TabIndex = 21
+        ' 
+        ' btnInfo
+        ' 
+        btnInfo.Location = New Point(737, 433)
+        btnInfo.Name = "btnInfo"
+        btnInfo.Size = New Size(99, 66)
+        btnInfo.TabIndex = 20
+        btnInfo.Text = "Info (?)"
+        btnInfo.UseVisualStyleBackColor = True
+        ' 
+        ' btnFilter
+        ' 
+        btnFilter.Location = New Point(728, 273)
+        btnFilter.Name = "btnFilter"
+        btnFilter.Size = New Size(110, 31)
+        btnFilter.TabIndex = 19
+        btnFilter.Text = "Cari Data"
+        btnFilter.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(496, 273)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(16, 25)
+        Label3.TabIndex = 18
+        Label3.Text = ":"
+        ' 
+        ' txtFilter
+        ' 
+        txtFilter.Location = New Point(518, 273)
+        txtFilter.Name = "txtFilter"
+        txtFilter.Size = New Size(191, 31)
+        txtFilter.TabIndex = 16
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(391, 273)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(84, 25)
+        Label4.TabIndex = 17
+        Label4.Text = "Cari Data"
+        ' 
+        ' btnBuatBaru
+        ' 
+        btnBuatBaru.Location = New Point(619, 433)
+        btnBuatBaru.Name = "btnBuatBaru"
+        btnBuatBaru.Size = New Size(99, 66)
+        btnBuatBaru.TabIndex = 15
+        btnBuatBaru.Text = "Data Baru"
+        btnBuatBaru.UseVisualStyleBackColor = True
+        ' 
+        ' btnBukaFile
+        ' 
+        btnBukaFile.Location = New Point(505, 433)
+        btnBukaFile.Name = "btnBukaFile"
+        btnBukaFile.Size = New Size(99, 66)
+        btnBukaFile.TabIndex = 14
+        btnBukaFile.Text = "Buka File"
+        btnBukaFile.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1148, 564)
+        Controls.Add(GroupBox1)
+        Name = "Form1"
+        Text = "Aplikasi Tabunganku"
+        CType(dgvRiwayat, ComponentModel.ISupportInitialize).EndInit()
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents dgvRiwayat As DataGridView
+    Friend WithEvents lblSaldo As Label
+    Friend WithEvents txtJumlah As TextBox
+    Friend WithEvents btnTambah As Button
+    Friend WithEvents btnRefresh As Button
+    Friend WithEvents ofdBukaFile As OpenFileDialog
+    Friend WithEvents btnHapus As Button
+    Friend WithEvents txtKeterangan As TextBox
+    Friend WithEvents lblInput As Label
+    Friend WithEvents lblKeterangan As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnBukaFile As Button
+    Friend WithEvents btnBuatBaru As Button
+    Friend WithEvents SaveFileDialog As SaveFileDialog
+    Friend WithEvents btnFilter As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtFilter As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btnInfo As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents cmbTransaksi As ComboBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents lblFileName As Label
+
+End Class
---
+++ tabunganku/Form1.vb
@@ -0,0 +1,235 @@
+﻿Imports System.IO
+
+Public Class Form1
+    ' Variabel saldo untuk menyimpan total saldo tabungan
+    Dim saldo As Decimal = 0
+    ' Path ke file CSV tempat menyimpan data tabungan
+    Dim filePath As String = ""
+
+    ' Inisialisasi DataGridView saat form dimuat
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Fokus awal pada ComboBox, memilih "Menabung"
+        cmbTransaksi.SelectedItem = "Menabung"
+
+        ' Menambahkan kolom pada DataGridView jika belum ada
+        If dgvRiwayat.Columns.Count = 0 Then
+            dgvRiwayat.Columns.Add("Tanggal", "Tanggal")
+            dgvRiwayat.Columns.Add("Jumlah", "Jumlah (Rp)")
+            dgvRiwayat.Columns.Add("Keterangan", "Keterangan")
+        End If
+    End Sub
+
+    ' Menambahkan tabungan ke file CSV
+    ' Menambahkan tabungan ke file CSV
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+        Dim jumlah As Decimal
+        Dim keterangan As String = txtKeterangan.Text.Trim()
+
+        ' Validasi jika input jumlah adalah angka yang valid dan lebih besar dari 0
+        If Decimal.TryParse(txtJumlah.Text, jumlah) AndAlso jumlah > 0 Then
+            ' Cek jenis transaksi yang dipilih (Menabung atau Penarikan)
+            If cmbTransaksi.SelectedItem = "Menabung" Then
+                saldo += jumlah
+                keterangan = "Menabung: " & keterangan ' Tambahkan keterangan untuk menabung
+            ElseIf cmbTransaksi.SelectedItem = "Penarikan" Then
+                ' Cek apakah saldo mencukupi untuk penarikan
+                If saldo >= jumlah Then
+                    saldo -= jumlah
+                    keterangan = "Penarikan: " & keterangan ' Tambahkan keterangan untuk penarikan
+                Else
+                    MessageBox.Show("Saldo tidak cukup untuk melakukan penarikan!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                    Return ' Jika saldo tidak cukup, proses dihentikan
+                End If
+            Else
+                MessageBox.Show("Pilih jenis transaksi (Menabung/Penarikan) terlebih dahulu!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                Return ' Jika jenis transaksi belum dipilih, proses dihentikan
+            End If
+
+            ' Menambahkan riwayat transaksi ke DataGridView
+            dgvRiwayat.Rows.Add(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), jumlah.ToString("N0"), keterangan)
+            ' Update label saldo
+            lblSaldo.Text = "Rp " & saldo.ToString("N0")
+            ' Tampilkan pesan sukses
+            MessageBox.Show("Transaksi berhasil!", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            ' Kosongkan input
+            txtJumlah.Clear()
+            txtKeterangan.Clear()
+
+            ' Tanyakan apakah ingin menyimpan file
+            If String.IsNullOrEmpty(filePath) Then
+                ' Jika filePath kosong, minta pengguna memilih file untuk menyimpan
+                Dim result As DialogResult = MessageBox.Show("Apakah Anda ingin menyimpan file tabungan?", "Simpan File", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+                If result = DialogResult.Yes Then
+                    btnBuatBaru_Click(sender, e) ' Panggil fungsi untuk membuat file baru jika belum ada
+                Else
+                    ' Menampilkan popup jika memilih No
+                    MessageBox.Show("Data tidak tersimpan!", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                End If
+            Else
+                ' Simpan data jika file sudah ada
+                SimpanData(jumlah, keterangan)
+            End If
+        Else
+            MessageBox.Show("Masukkan jumlah yang valid!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
+
+
+    ' Prosedur untuk menyimpan data transaksi ke file CSV
+    Private Sub SimpanData(jumlah As Decimal, keterangan As String)
+        Dim isNewFile As Boolean = Not File.Exists(filePath)
+
+        ' Menulis data ke file CSV
+        Using writer As StreamWriter = New StreamWriter(filePath, True)
+            ' Jika file baru, tambahkan header
+            If isNewFile Then writer.WriteLine("Tanggal,Jumlah (Rp),Keterangan")
+            ' Menulis transaksi ke file
+            writer.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") & "," & jumlah & "," & keterangan)
+        End Using
+    End Sub
+
+    ' Membaca dan menampilkan data dari file CSV
+    Private Sub btnRefresh_Click(sender As Object, e As EventArgs) Handles btnRefresh.Click
+        BacaData()
+    End Sub
+
+    ' Prosedur untuk membaca dan menampilkan data dari file CSV
+    Private Sub BacaData(Optional customFilePath As String = "")
+        Dim targetFile As String = If(String.IsNullOrEmpty(customFilePath), filePath, customFilePath)
+
+        ' Cek apakah file ada
+        If Not File.Exists(targetFile) Then
+            MessageBox.Show("File tidak ditemukan!", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Return
+        End If
+
+        ' Cek apakah file kosong
+        If New FileInfo(targetFile).Length = 0 Then
+            MessageBox.Show("File kosong, tidak ada data untuk ditampilkan.", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Return
+        End If
+
+        dgvRiwayat.Rows.Clear()
+        saldo = 0
+
+        ' Membaca data dari file CSV
+        Using reader As New StreamReader(targetFile)
+            Dim headerSkipped As Boolean = False
+
+            While Not reader.EndOfStream
+                Dim line As String = reader.ReadLine()
+                Dim data As String() = line.Split(","c)
+
+                ' Lewati baris pertama jika itu header
+                If Not headerSkipped Then
+                    headerSkipped = True
+                    Continue While
+                End If
+
+                ' Validasi data yang dibaca dan menambahkannya ke DataGridView
+                If data.Length = 3 AndAlso IsNumeric(data(1)) Then
+                    dgvRiwayat.Rows.Add(data(0), Convert.ToDecimal(data(1)).ToString("N0"), data(2))
+                    saldo += Convert.ToDecimal(data(1)) ' Menambah saldo berdasarkan data yang dibaca
+                End If
+            End While
+        End Using
+
+        ' Update label saldo setelah membaca file
+        lblSaldo.Text = "Rp " & saldo.ToString("N0")
+    End Sub
+
+    ' Membuka file CSV lain
+    Private Sub btnBukaFile_Click(sender As Object, e As EventArgs) Handles btnBukaFile.Click
+        Using openFileDialog As New OpenFileDialog()
+            openFileDialog.Filter = "CSV Files (*.csv)|*.csv|All Files (*.*)|*.*"
+            openFileDialog.Title = "Pilih File Tabungan"
+
+            If openFileDialog.ShowDialog() = DialogResult.OK Then
+                filePath = openFileDialog.FileName
+                ' Menampilkan nama file di label
+                lblFileName.Text = "File: " & Path.GetFileName(filePath)
+                BacaData(filePath)
+            End If
+        End Using
+    End Sub
+
+    ' Membuat file tabungan baru
+    Private Sub btnBuatBaru_Click(sender As Object, e As EventArgs) Handles btnBuatBaru.Click
+        Using saveFileDialog As New SaveFileDialog()
+            saveFileDialog.Filter = "CSV Files (*.csv)|*.csv|All Files (*.*)|*.*"
+            saveFileDialog.Title = "Buat File Tabungan Baru"
+
+            If saveFileDialog.ShowDialog() = DialogResult.OK Then
+                filePath = saveFileDialog.FileName
+                ' Menulis header CSV jika file baru
+                File.WriteAllText(filePath, "Tanggal,Jumlah (Rp),Keterangan" & Environment.NewLine)
+                dgvRiwayat.Rows.Clear()
+                saldo = 0
+                lblSaldo.Text = "Rp 0"
+                MessageBox.Show("File tabungan baru berhasil dibuat!", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End If
+        End Using
+    End Sub
+
+    ' Menghapus baris yang dipilih di DataGridView dan memperbarui file CSV
+    Private Sub btnHapus_Click(sender As Object, e As EventArgs) Handles btnHapus.Click
+        If dgvRiwayat.SelectedRows.Count > 0 Then
+            Dim result As DialogResult = MessageBox.Show("Apakah Anda yakin ingin menghapus data yang dipilih?", "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Warning)
+            If result = DialogResult.Yes Then
+                For Each row As DataGridViewRow In dgvRiwayat.SelectedRows.Cast(Of DataGridViewRow).ToArray()
+                    If Not row.IsNewRow Then
+                        dgvRiwayat.Rows.Remove(row) ' Menghapus baris yang dipilih
+                    End If
+                Next
+                SimpanUlangCSV()
+                MessageBox.Show("Data berhasil dihapus!", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                BacaData() ' Memperbarui data setelah penghapusan
+            End If
+        Else
+            MessageBox.Show("Pilih data yang ingin dihapus!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
+
+    ' Prosedur menyimpan ulang CSV setelah penghapusan data
+    Private Sub SimpanUlangCSV()
+        Using writer As StreamWriter = New StreamWriter(filePath, False)
+            writer.WriteLine("Tanggal,Jumlah (Rp),Keterangan") ' Menulis ulang header
+            For Each row As DataGridViewRow In dgvRiwayat.Rows
+                If Not row.IsNewRow Then
+                    writer.WriteLine(row.Cells(0).Value.ToString() & "," & row.Cells(1).Value.ToString() & "," & row.Cells(2).Value.ToString())
+                End If
+            Next
+        End Using
+    End Sub
+
+    ' Filter data berdasarkan semua kolom
+    Private Sub btnFilter_Click(sender As Object, e As EventArgs) Handles btnFilter.Click
+        Dim keyword As String = txtFilter.Text.Trim().ToLower()
+        For Each row As DataGridViewRow In dgvRiwayat.Rows
+            If Not row.IsNewRow Then
+                Dim rowData As String = row.Cells(0).Value.ToString().ToLower() & " " & row.Cells(1).Value.ToString().ToLower() & " " & row.Cells(2).Value.ToString().ToLower()
+                row.Visible = rowData.Contains(keyword) ' Menyembunyikan baris yang tidak sesuai dengan keyword
+            End If
+        Next
+    End Sub
+
+    ' Pop-up panduan penggunaan aplikasi
+    Private Sub btnInfo_Click(sender As Object, e As EventArgs) Handles btnInfo.Click
+        Dim panduan As String = "Panduan Penggunaan Aplikasi Tabunganku:" & vbCrLf & vbCrLf &
+                            "1. Pilih jenis transaksi (Menabung/Penarikan) pada menu pilihan." & vbCrLf &
+                            "2. Masukkan jumlah tabungan dan keterangan, lalu klik 'Tambah'." & vbCrLf &
+                            "   - Jika memilih 'Menabung', jumlah akan ditambahkan ke saldo." & vbCrLf &
+                            "   - Jika memilih 'Penarikan', jumlah akan dikurangi dari saldo, asalkan saldo cukup." & vbCrLf &
+                            "3. Pilih data lalu klik 'Hapus' untuk menghapus tabungan." & vbCrLf &
+                            "4. Gunakan 'Buka File' untuk membuka data tabungan lain." & vbCrLf &
+                            "5. Klik 'Buat Baru' untuk membuat file tabungan baru." & vbCrLf &
+                            "6. Gunakan fitur filter untuk mencari data lebih cepat."
+
+        MessageBox.Show(panduan, "Panduan Penggunaan", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    ' Menyimpan perubahan data ketika edit sel di DataGridView
+    Private Sub dgvRiwayat_CellEndEdit(sender As Object, e As DataGridViewCellEventArgs) Handles dgvRiwayat.CellEndEdit
+        SimpanUlangCSV() ' Menyimpan data setelah perubahan di DataGridView
+    End Sub
+End Class
